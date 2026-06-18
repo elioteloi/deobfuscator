019bff00ba8055bdd4f8ff1fc2d5f3909ec8442c
Cadu-f12 <cadudailha68@gmail.com>
2026-06-18 09:16:27 -0300

    feat: Implement the decoder.c

--- decoder.h
+++ decoder.h
@@ -1,4 +1,9 @@
-char *decoder(char **encoded_text);
+#ifndef DECODER_H
+#define DECODER_H
+
+#include "parser.h"
+
+char *decoder(Token *encoded_text, int count);
 
 char decode_html_hexadecimal(char *token);
 
@@ -7,3 +12,7 @@ char decode_percent_encouding(char *token);
 char decode_html_decimal(char *token);
 
 char decode_char_sequence(char *token);
+
+char *decode_html_entity(char *token);
+
+#endif
