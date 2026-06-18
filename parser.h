916566991e622644807a1e3379736dc0c47c8994
Cadu-f12 <cadudailha68@gmail.com>
2026-06-18 09:15:09 -0300

    fix: Fixing the parser function

--- parser.h
+++ parser.h
@@ -1,11 +1,16 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-struct parser {
+#define PERCENT 0
+#define HTML 1
+#define CHAR 2
+#define LETTER 3
+
+typedef struct parser {
     char data[20];
     int type;
-};
+} Token;
 
-struct parser *parsing(int size, char *string, int *count);
+Token *parsing(int size, char *string, int *count);
 
 #endif
